f8d86ecd0b8c8c61b96990ba2b908ece02fdd0b6
chenjunlin <1739560479@qq.com>
2024-05-14 20:58:04 +0800

    aop逻辑加入缓存优化

--- neutrino-core/src/main/java/fun/asgc/neutrino/core/aop/Aop.java
+++ neutrino-core/src/main/java/fun/asgc/neutrino/core/aop/Aop.java
@@ -4,19 +4,27 @@ package fun.asgc.neutrino.core.aop;
 import fun.asgc.neutrino.core.aop.proxy.Proxy;
 import fun.asgc.neutrino.core.aop.proxy.ProxyFactory;
 import fun.asgc.neutrino.core.aop.proxy.ProxyStrategy;
+import fun.asgc.neutrino.core.cache.Cache;
+import fun.asgc.neutrino.core.cache.MemoryCache;
 import fun.asgc.neutrino.core.util.Assert;
+import fun.asgc.neutrino.core.util.LockUtil;
 
 /**
  *
- * @author: chenjunlin
- * @date: 2024/5/14
+ * &#064;author:  chenjunlin
+ * &#064;date:  2024/5/14
  */
 public class Aop {
 	private static final ProxyStrategy proxyStrategy = ProxyStrategy.SUB_CLASS_PROXY;
 	private static final ProxyFactory proxyFactory = Proxy.getProxyFactory(proxyStrategy);
+	private static final Cache<Class<?>, Object> proxyBeanCache = new MemoryCache<>();
 
 	public static <T> T get(Class<T> clazz) {
 		Assert.notNull(proxyFactory, "代理工厂初始化异常!");
-		return proxyFactory.get(clazz);
+		return (T)LockUtil.doubleCheckProcess(() -> !proxyBeanCache.containsKey(clazz),
+				clazz,
+				() -> proxyBeanCache.set(clazz, proxyFactory.get(clazz)),
+				() -> proxyBeanCache.get(clazz)
+		);
 	}
 }
